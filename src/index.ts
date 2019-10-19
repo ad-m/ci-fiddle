6ae787017aa8fb976d95e5dcbee85934392677fa
Jeroen de Bruijn <vidavidorra@gmail.com>
2019-10-19 11:05:14 +0200

    chore: add

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export * from './global-linters';
-export * from './linter';
-export * from './glob';
+export * from "./global-linters";
+export * from "./linter";
+export * from "./glob";
+console.log("lals");
