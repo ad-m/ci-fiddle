c0ab6d6dee5d43fbe6630c34ba3f1d497f449bbc
Jeroen de Bruijn <vidavidorra@gmail.com>
2019-12-04 20:25:27 +0100

    chore: cck

--- .versionrc.js
+++ .versionrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   scripts: {
-    prerelease:
-      'if [ "$(git rev-parse --abbrev-ref HEAD)" != "master" ]; then exit 9; fi',
+    prerelease: [
+      'if [[ "${GITHUB_ACTIONS}" != "true" ]] &&',
+      '[[ "$(git branch --show-current)" != "master" ]];',
+      'then exit -1; fi',
+    ].join(' '),
   },
 };
