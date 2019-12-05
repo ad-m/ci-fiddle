d66a6e72b6aa1491546a02dc55bf3a58281d808a
Jeroen de Bruijn <vidavidorra@gmail.com>
2019-12-05 20:03:15 +0100

    fix: no version

--- .versionrc.js
+++ .versionrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   scripts: {
-    prerelease: [
-      'if [[ "${GITHUB_ACTIONS}" != "true" ]] &&',
-      '[[ "$(git branch --show-current)" != "master" ]];',
-      'then exit -1; fi',
-    ].join(' '),
+    // prerelease: [
+    //   'if [[ "${GITHUB_ACTIONS}" != "true" ]] &&',
+    //   '[[ "$(git branch --show-current)" != "master" ]];',
+    //   'then exit -1; fi',
+    // ].join(' '),
   },
 };
