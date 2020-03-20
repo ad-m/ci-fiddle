43dfd500d850d9be94059f2bca8cfb38ebbdf012
Jeroen de Bruijn <vidavidorra@gmail.com>
2020-03-20 22:07:01 +0100

    chore: push Fri 20 Mar 22:07:01 CET 2020

---
+++ example/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branchPrefix: 'ga-renovate/',
+  dryRun: true,
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  logLevel: 'debug',
+  onboarding: false,
+  platform: 'github',
+  repositories: ['vidavidorra/github-action-renovate'],
+};
--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -6,19 +6,23 @@ readonly CONFIGURATION_FILE="${1}"
 readonly TOKEN="${2}"
 
 readonly CONFIGURATION_PATH="${GITHUB_WORKSPACE}/${CONFIGURATION_FILE}"
+
 if [[ ! -f "${CONFIGURATION_PATH}" ]]; then
   echo "ERROR: Couldn't find file ${CONFIGURATION_PATH}" 1>&2
   exit 1
 fi
 
-cp "${CONFIGURATION_PATH}" '/usr/src/app/config.js'
+readonly WORKDIR='/usr/src/app'
+# Account for the WORKDIR entry of the renovate/renovate Docker container. See
+# the followling link for this entry.
+# https://github.com/renovatebot/renovate/blob/19.175.3/Dockerfile#L14
+cd "${WORKDIR}" || { echo "ERROR: Couldn't cd to ${WORKDIR}" 1>&2; exit 1; }
 
-cd /usr/src/app || { echo "ERROR: Couldn't cd to /usr/src/app" 1>&2; exit 1; }
+cp "${CONFIGURATION_PATH}" './config.js'
 
 # Run Renovate.
 #
-# This mimics the original entrypoint of the renovate/renovate Docker container.
-# See the following link for this entrypoint in the renovate/renovate Docker
-# container.
+# Mimic the original ENTRYPOINT of the renovate/renovate Docker container. See
+# the following link for this entry.
 # https://github.com/renovatebot/renovate/blob/19.175.3/Dockerfile#L220
 RENOVATE_TOKEN="${TOKEN}" node /usr/src/app/dist/renovate.js
