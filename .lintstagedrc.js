b4803b400c945a2d014e43965b70fe757adc5d7b
Jeroen de Bruijn <vidavidorra@gmail.com>
2020-03-21 14:11:50 +0100

    chore: push Sat 21 Mar 14:11:50 CET 2020

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,6 @@ module.exports = {
   '*.{ts,tsx,js,jsx,json}': [
     'npm run format-file:default',
     'npm run lint',
-    'npm run test:staged',
     'git add',
   ],
   '*.{js,jsx,vue,ts,css,less,scss,html,htm,json,md,markdown,yml,yaml}': [
