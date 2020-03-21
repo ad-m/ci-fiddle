306e36af9a5293e1ec03bfad440e39d3fffe1c9d
Jeroen de Bruijn <vidavidorra@gmail.com>
2020-03-21 22:45:20 +0100

    chore(release): trigger release process [ci release]

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,11 +1,6 @@
 module.exports = {
-  '*.{ts,tsx,js,jsx,json}': [
-    'npm run format-file:default',
-    'npm run lint',
-    'git add',
-  ],
+  '*.{ts,tsx,js,jsx,json}': ['npm run format-file:default', 'npm run lint'],
   '*.{js,jsx,vue,ts,css,less,scss,html,htm,json,md,markdown,yml,yaml}': [
     'npm run format-file:default',
-    'git add',
   ],
 };
